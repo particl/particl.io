cdf687136a50a308d88f01bb2297f4d2fdfa5f45
Martin Allien <martin@allien.work>
2019-12-03 14:36:41 +0100

    Gulpfile tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -36,11 +36,23 @@ const server = browserSync.create();
 const fontName = 'icons';
 
 const paths = {
-  template: '*.html',
+  template: [
+    './**/*.html',
+    './**/*.md'
+  ],
   // CSS
-  scss: './_sass/**/*.scss',
+  scss: './_sass/*.scss',
   css: './assets/css',
+  styles: {
+    input: './_sass/*.scss',
+    output: './assets/css'
+  },
   // JS
+  scripts: {
+    folder: './assets/js/src/',
+    input: './assets/js/src/*.js',
+    output: './assets/js',
+  },
   js: './assets/js/src/',
   js_in: './assets/js/src/*.js',
   js_out: './assets/js',
@@ -54,40 +66,43 @@ const paths = {
 /* ------------------------------------ *\
     Tasks
     Gulp 3 => 4 guides:
+      - https://www.goede.site/setting-up-gulp-4-for-automatic-sass-compilation-and-css-injection
       - https://www.joezimjs.com/javascript/complete-guide-upgrading-gulp-4/
       - https://coder-coder.com/gulp-4-walk-through/
       - https://github.com/gulpjs/gulp/blob/master/docs/recipes/minimal-browsersync-setup-with-gulp4.md
 \* ------------------------------------ */
 
 function styles(){
-  return src([paths.scss], {base: '.'})
+  return (
+    src(paths.styles.input)
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({cascade: false}))
     .pipe(sourcemaps.write(''))
-    .pipe(dest(paths.css)
+    .pipe(dest(paths.styles.output))
     .pipe(browserSync.stream())
   );
 }
 
 
 function scripts(){
-  return src([
-    paths.js + 'jquery-1.11.2.min.js',
-    paths.js + 'modernizr.min.js',
-    paths.js + 'owl.carousel.min.js',
-    paths.js + 'jquery.countdown.min.js', // http://hilios.github.io/jQuery.countdown/
-    paths.js + 'moment.min.js', // countdown-related
-    paths.js + 'moment-timezone-with-data-2012-2022.min.js', // countdown-related
-    paths.js + 'jquery.magnific-popup.min.js', // pop-ups
-    paths.js + 'aos.js', // AnimateOnScroll
-    paths.js + 'particl.js',
-  ])
+  return (
+    src([
+      paths.scripts.folder + 'jquery-1.11.2.min.js',
+      paths.scripts.folder + 'modernizr.min.js',
+      paths.scripts.folder + 'owl.carousel.min.js',
+      paths.scripts.folder + 'jquery.countdown.min.js', // http://hilios.github.io/jQuery.countdown/
+      paths.scripts.folder + 'moment.min.js', // countdown-related
+      paths.scripts.folder + 'moment-timezone-with-data-2012-2022.min.js', // countdown-related
+      paths.scripts.folder + 'jquery.magnific-popup.min.js', // pop-ups
+      paths.scripts.folder + 'aos.js', // AnimateOnScroll
+      paths.scripts.folder + 'particl.js',
+    ])
     .pipe(sourcemaps.init())
     .pipe(concat('all.js'))
     .pipe(uglify())
     .pipe(sourcemaps.write(''))
-    .pipe(dest(paths.js_out)
+    .pipe(dest(paths.scripts.output))
     .pipe(browserSync.stream())
   );
 }
@@ -115,7 +130,6 @@ function icons(){
   ;
 }
 
-
 // BrowserSync - start server
 function serve(done) {
   server.init({
@@ -127,28 +141,22 @@ function serve(done) {
   done();
 }
 
+
 // BrowserSync - reload page
 function reload(done) {
   server.reload();
   done();
 }
 
-// Watch task
-function watcher(){
-  watch([
-    paths.scss,
-    paths.js_in,
-    paths.template
-  ],
-    parallel(
-      styles,
-      scripts,
-      reload
-    )
-  );
+
+
+function watcher() {
+  watch(paths.styles.input, styles);
+  watch(paths.scripts.input, scripts);
+  watch(paths.template, reload);
 }
 
 
 // Default task
-exports.default = series( parallel(styles, scripts, serve), watcher );
+exports.default = series( parallel( styles, scripts, serve ), watcher );
 exports.webfont = series( icons );
